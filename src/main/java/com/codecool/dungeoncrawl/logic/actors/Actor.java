498aef722216e1555c3b1593f2913f5c8b958a35
RafalGontarski <gontarskirafal@gmail.com>
2023-01-11 11:56:35 +0100

    0.3 Actually merged. Restrict movement works.

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -8,42 +8,17 @@ import com.codecool.dungeoncrawl.logic.utils.MathHelper;
 
 import java.awt.*;
 
-public class Actor extends Rectangle implements Drawable {
+public abstract class Actor implements Drawable {
     private final ActorType actorType;
     private Cell cell;
     protected int speed;
-    protected byte entityID;
-
-    protected boolean up;
-    protected boolean down;
-    protected boolean left;
-    protected boolean right;
     private int health = 10;
-    protected MathHelper.Direction facing;
-    protected byte animationFrame;
-    protected byte animationDelay;
-
-
 
     public Actor(Cell cell, ActorType actorType) {
         this.cell = cell;
         this.actorType = actorType;
         this.cell.setActor(this);
         this.speed = 5;
-    }
-
-
-    public Actor(byte id, int posXinRoom, int posYinRoom) {
-        super(posXinRoom* Tiles.SIZE, posYinRoom*Tiles.SIZE, Tiles.SIZE, Tiles.SIZE);
-        this.entityID = id;
-        this.up = false;
-        this.down = false;
-        this.left = false;
-        this.right = false;
-        this.speed = 5;
-        this.facing = MathHelper.Direction.SOUTH;
-        this.animationFrame = 0;
-        this.actorType = getActorType();
     }
 
     public void move(int dx, int dy) {
@@ -56,56 +31,7 @@ public class Actor extends Rectangle implements Drawable {
                 cell = nextCell;
             }
         }
-
-
-        if(up) {
-            super.y-=this.speed;
-            this.facing = MathHelper.Direction.NORTH;
-        }
-        if(down) {
-            super.y+=this.speed;
-            this.facing = MathHelper.Direction.SOUTH;
-        }
-        if(left) {
-            super.x-=this.speed;
-            this.facing = MathHelper.Direction.WEST;
-        }
-        if(right) {
-            super.x+=this.speed;
-            this.facing = MathHelper.Direction.EAST;
-        }
     }
-
-    public void setMovingUp(boolean up) {
-        this.up = up;
-    }
-
-    public void setMovingDown(boolean down) {
-        this.down = down;
-    }
-
-    public void setMovingLeft(boolean left) {
-        this.left = left;
-    }
-
-    public void setMovingRight(boolean right) {
-        this.right = right;
-    }
-
-    public void setCenterX(int x) {
-        super.x = x - super.width/2;
-    }
-
-    public void setCenterY(int y) {
-        super.y = y - super.height/2;
-    }
-
-//    public void move(int dx, int dy) {
-//        Cell nextCell = cell.getNeighbor(dx, dy);
-//        cell.setActor(null);
-//        nextCell.setActor(this);
-//        cell = nextCell;
-//    }
 
     @Override
     public String getTileName() {
@@ -124,11 +50,4 @@ public class Actor extends Rectangle implements Drawable {
         return cell;
     }
 
-//    public int getX() {
-//        return cell.getX();
-//    }
-//
-//    public int getY() {
-//        return cell.getY();
-//    }
 }
--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -1,33 +1,16 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
-import com.codecool.dungeoncrawl.logic.utils.Config;
-import com.codecool.dungeoncrawl.logic.utils.MathHelper;
 
 public class Skeleton extends Actor {
     private Actor actor;
     private Player target;
-
-    private int hp;
+    private int _hp;
 
     public Skeleton(Cell cell, ActorType actorType) {
         super(cell, actorType);
         super.speed = 2;
-        this.hp = getHealth();
-    }
-
-
-    public Skeleton(byte id, int health, Player target) {
-        super(id, Config.ENEMY_HP, Config.ENEMY_HIT);
-        this.target = target;
-        super.speed = 2;
-        this.hp = health;
-    }
-
-    public void damage(int amount, MathHelper.Direction hit) {
-        this.hp -= amount;
-        super.x += hit.dirX * 90;
-        super.y += hit.dirY * 90;
+        this._hp = 5;
     }
 
     @Override
